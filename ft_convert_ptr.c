d61a8afcda92129992046be790e246275bc7ccfe
Philipp Rollinger <philipp.rollinger@gmail.com>
2021-08-04 11:07:53 +0200

    (nil) ptr return done

--- ft_convert_ptr.c
+++ ft_convert_ptr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_convert_ptr.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: prolling <prolling@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*   By: prolling <prolling@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/12 11:16:16 by prolling          #+#    #+#             */
-/*   Updated: 2021/07/13 10:40:08 by prolling         ###   ########.fr       */
+/*   Updated: 2021/08/04 11:06:52 by prolling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@
 */
 void	ft_convert_ptr(t_format *format)
 {
-	format->str = ft_strfjoin(ft_strdup("0x"), ft_uitox(format->var_ulli));
+	if (format->var_ulli == 0)
+		format->str = ft_strdup("(nil)");//ft_strfjoin(ft_strdup(""), ft_strdup("(nil)"));
+	else
+		format->str = ft_strfjoin(ft_strdup("0x"), ft_uitox(format->var_ulli));
 	return ;
 }
